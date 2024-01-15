cceefd500a587afefd11e03f16709307c4af18af
Jonathan Matarazzi <git@jonathanm.nl>
2024-01-15 22:44:10 +0100

    Fix connection lines rotation

--- resources/js/Components/Vierkandle/Connection.vue
+++ resources/js/Components/Vierkandle/Connection.vue
@@ -3,6 +3,8 @@ const props = defineProps<{
     start: DOMRect,
     end: DOMRect,
 }>()
+
+console.log(props.start, props.end)
 </script>
 
 <template>
@@ -12,7 +14,7 @@ const props = defineProps<{
     <div v-else-if="start.x == end.x" class="ver connection fixed w-8"
          :style="`left: ${start.left+start.width/2}px; top: ${Math.min(start.y+start.height/2, end.y+end.height/2)}px; height: ${Math.abs(end.y-start.y)}px;`">
     </div>
-    <div v-else-if="start.x-end.x == start.y-end.y" class="connection diag-1 fixed h-8"
+    <div v-else-if="Math.abs((start.x-end.x) - (start.y-end.y)) < 1" class="connection diag-1 fixed h-8"
          :style="`left: ${Math.min(start.x+start.width/2, end.x+end.width/2)}px; top: ${Math.min(start.y+start.height/2, end.y+end.height/2)}px; width: ${Math.sqrt(Math.pow(end.x-start.x,2)*2)}px;`">
     </div>
     <div v-else class="connection diag-2 fixed h-8"
