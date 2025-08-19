0f81331a235508d35c169e6387e7f3aff0f9f803
Jonathan Matarazzi <git@jonathanm.nl>
2025-08-19 23:27:00 +0200

    Update cron.sh to new deployment location

--- cron.sh
+++ cron.sh
@@ -1,1 +1,1 @@
-(cd /home/u23515p17945/domains/vierkandle.nl/vierkandle && /opt/alt/php81/usr/bin/php artisan schedule:run)
+(cd /home/u23515p17945/domains/vierkandle.nl/deployment/live && /opt/alt/php82/usr/bin/php artisan schedule:run)
